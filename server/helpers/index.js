6aba9e9af5b156c1f25d12d97560b9e1427bda58
Musigwa <pacifique.musigwa@gmail.com>
2018-11-27 11:29:49 +0200

    [Feature #162114302] verify user login info

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,8 +1,6 @@
 import jwt from 'jsonwebtoken';
 
 export default class Helpers {
-  constractor() {}
-
   static createToken(args = {}, options = {}) {
     return jwt.sign(args, process.env.JWT_KEY, options);
   }
